6736c53a14df7af2c56c0c37c0d8334577780236
hwang <HHwang@qaconsultants.com>
2021-12-01 09:39:50 -0500

    add console.h and console.cpp

---
+++ vs2022FirstApp/console.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef MURACH_CONSOLE_H
+#define MURACH_CONSOLE_H
+
+#include <string>
+#include <limits>
+
+namespace console {
+	double get_double(std::string prompt,
+		double min = std::numeric_limits<double>::min(),
+		double max = std::numeric_limits<double>::max());
+	int get_int(std::string prompt,
+		int min = std::numeric_limits<int>::min(),
+		int max = std::numeric_limits<int>::max());
+	bool get_char(std::string prompt, bool add_blank_line = true);
+}
+#endif // !MURACH_CONSOLE_H
+
