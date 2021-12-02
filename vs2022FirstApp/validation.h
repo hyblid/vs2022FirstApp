2437a43624b46e2798e8e06bd5d5b5dfc9b71426
hwang <HHwang@qaconsultants.com>
2021-12-01 21:39:21 -0500

    add validation.h and validation.cpp

---
+++ vs2022FirstApp/validation.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef HOWARD_VaLIDATION_H
+#include <string>
+namespace validation {
+	bool is_valid_password(std::string pw);
+	bool is_valid_email(std::string email);
+}
+#endif // !HOWARD_VaLIDATION_H
