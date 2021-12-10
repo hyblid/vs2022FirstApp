0aa1b91ec0313200941308db907ec2b720a23a75
hwang <HHwang@qaconsultants.com>
2021-12-10 10:08:09 -0500

    EX10-2 use Map collection

--- vs2022FirstApp/vs2022FirstApp.cpp
+++ vs2022FirstApp/vs2022FirstApp.cpp
@@ -14,6 +14,7 @@
 #include "validation.h"
 #include <list>
 #include "Sample.cpp"
+#include <map>
 
 import helloWorld;
 using std::cin;
@@ -115,190 +116,56 @@ ostream& operator<<(ostream& COUT, Sample& sample) {
 	return COUT;
 }
 
-struct Movie {
-    string title = "";
-    unsigned int year = 0;
-    unsigned int stars = 0;
-    bool equals(Movie&);              // member function declaration
-};
-
-// member function definition
-bool Movie::equals(Movie& to_compare) {
-    return (title == to_compare.title && year == to_compare.year);
+char get_choice() {
+	char choice;
+	cout << "Continue (y/n)?: ";
+	cin >> choice;
+	cout << endl;
+	return choice;
 }
 
-const string movies_file = "movies.txt";
-
-list<Movie> read_movies_from_file() {
-    list<Movie> movies;
-
-    ifstream input_file(movies_file);
-    if (input_file) {    // if file opened successfully...
-        Movie movie;
-        string line;
-        while (getline(input_file, line)) {
-            stringstream ss(line);
-
-            getline(ss, movie.title, '\t');     // get title
-            ss >> movie.year >> movie.stars;    // get year and stars
-            movies.push_back(movie);            // add movie to vector
-        }
-        input_file.close();
-    }
-    return movies;
+void display_codes(const map<string, string>& countries) {
+	cout << "Country codes: ";
+	for (auto country : countries) {
+		cout << country.first << " ";
+	}
+	cout << endl << endl;
 }
 
-void write_movies_to_file(const list<Movie>& movies) {
-    ofstream output_file(movies_file);
-    if (output_file) {     // if file opened successfully...
-        for (Movie movie : movies) {
-            output_file << movie.title << '\t'
-                << movie.year << '\t'
-                << movie.stars << '\n';
-        }
-        output_file.close();
-    }
+void display_country(const map<string, string>& countries) {
+	string code;
+	cout << "Enter a country code: ";
+	cin >> code;
+	string upper_code = "";
+	for (char c : code) {
+		upper_code += toupper(c);
+	}
+
+	// get iterator for map element with code
+	auto itr = countries.find(code);
+	// if the code isn't found, display a message
+	// if the code is found, display the country name
+	if (itr == countries.end()) {
+		cout << "Country code not found" << endl;
+	}
+	else {
+		cout << "You selected " << itr->second << endl << endl;
+	}
 }
 
-void view_movies(const list<Movie>& movies) {
-    int col_width = 8;
-    cout << left
-        << setw(col_width / 2) << " "
-        << setw(col_width * 4) << "TITLE"
-        << setw(col_width) << "YEAR"
-        << setw(col_width) << "STARS" << endl;
+int main()
+{
+	map<string, string> countries{ {"CA", "Canada"}, {"US", "United States"}, {"MX", "Mexico"} };
 
-    int number = 1;
-    for (Movie movie : movies) {
-        cout << setw(col_width / 2) << number
-            << setw(col_width * 4) << movie.title
-            << setw(col_width) << movie.year
-            << setw(col_width) << movie.stars << endl;
-        ++number;
-    }
-    cout << endl;
-}
-
-Movie get_movie() {
-    Movie movie;
-    cout << "Title: ";
-    cin.ignore(1000, '\n');
-    getline(cin, movie.title);
-    cout << "Year: ";
-    cin >> movie.year;
-    cout << "Stars (1-5): ";
-    cin >> movie.stars;
-    return movie;
-}
-
-void add_movie(list<Movie>& movies) {
-    Movie movie = get_movie();
-
-    // check if movie already exists
-    bool already_exists = false;
-    for (Movie& m : movies) {
-        if (m.equals(movie)) {
-            if (already_exists = true) {
-                cout << movie.title << " already exists and was updated.\n\n";
-            }
-
-            m.stars = movie.stars;
-            break;
-        }
-    }
-
-    if (already_exists) {
-        write_movies_to_file(movies);
-    }
-    else {
-        movies.push_back(movie);
-        write_movies_to_file(movies);
-        cout << movie.title << " was added.\n\n";
-    }
-}
-
-int get_movie_number(const list<Movie>& movies) {
-    cin.ignore(1000, '\n');
-    int number;
-    while (true) {
-        cout << "Number: ";
-        cin >> number;
-        if (number > 0 && number <= movies.size()) {
-            return number;
-        }
-        else {
-            cout << "Invalid movie number. Try again.\n";
-        }
-    }
-}
-
-void delete_movie(list<Movie>& movies) {
-    int number = get_movie_number(movies);
-    auto itr = movies.begin();
-    for (int i = 0; i < number -1; i++)
-    {
-        itr++;
-    }
-    Movie movie = *itr;
-    movies.erase(itr);
-    write_movies_to_file(movies);
-    cout << movie.title << " was deleted.\n\n";
-}
-
-void display_menu() {
-    cout << "COMMANDS\n"
-        << "v - View movie list\n"
-        << "a - Add a movie\n"
-        << "d - Delete a movie\n"
-        << "m - Modify a movie\n"
-        << "x - Exit\n\n";
-}
-
-void modify_movie(list<Movie>& movies) {
-    auto itr = movies.begin();
-    int rating = 0;
-    int number = get_movie_number(movies);
-
-    for (int i = 0; i < number -1 ; i++)
-    {
-        itr++;
-    }
-    Movie movie = *itr;
-    cout << "Enter new number of stars (1-5) for " << itr->title << ": ";
-    cin >> rating;
-    itr->stars = rating;
-    write_movies_to_file(movies);
-    cout << movie.title << " was updated.\n\n";
-}
-
-int main() {
-    cout << "The Movie List program\n\n";
-    display_menu();
-    list<Movie> movies = read_movies_from_file();
-    int rate = 0;
-    char command = 'v';
-    while (command != 'x') {
-        cout << "Command: ";
-        cin >> command;
-        switch (command) {
-        case 'v':
-            view_movies(movies);
-            break;
-        case 'a':
-            add_movie(movies);
-            break;
-        case 'd':
-            delete_movie(movies);
-            break;       
-        case 'm':
-            modify_movie(movies);
-            break;
-        case 'x':
-            cout << "Bye!\n\n";
-            break;
-        default:
-            cout << "Not a valid command. Please try again.\n\n";
-            break;
-        }
-    }
+	cout << "The Country Codes Program\n\n";
+	char choice = 'y';
+	display_codes(countries);
+	while (choice == 'y') {
+		display_country(countries);
+		cout << "Continue (y/n)?: ";
+		cin >> choice;
+		cin.ignore(1000, '\n');
+	}
+	cout << "Bye!" << endl;
+	// while the user indicates they want to continue, display a country
 }
