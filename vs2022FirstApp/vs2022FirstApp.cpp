f717eb4e5e830f88b44d4dad71ec0b9c4857fb33
hwang <HHwang@qaconsultants.com>
2021-12-22 08:37:57 -0500

    finish EX19-2 and add list.h, MyIterator.h and MyVector.h

--- vs2022FirstApp/vs2022FirstApp.cpp
+++ vs2022FirstApp/vs2022FirstApp.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include "MyVector.h"
+#include "MyList.h"
 
 using namespace std;
 
 template<typename T>
-void display(MyVector<T> elements) {   // test copy constructor
+void display(MyList<T>& elements) {   // don't test copy constructor
     for (T elem : elements) {
         cout << elem << '|';
     }
@@ -14,27 +13,33 @@ void display(MyVector<T> elements) {   // test copy constructor
 }
 
 int main() {
-    cout << "TESTING\n";
-
-    // test initialization list constructor
-    MyVector<string> tasks{ "Go to store", "Feed cats", "Check email" };
-    cout << "Init:        ";
-    display(tasks);                 // test copy constructor
-    cout << "capacity():  " << tasks.capacity() << endl;
-
     // test push_back() function
-    cout << "push_back(): ";
+    cout << "TESTING\n";
+    MyList<string> tasks;
+    tasks.push_back("Go to store");
+    tasks.push_back("Feed cats");
+    tasks.push_back("Check email");
+    tasks.push_back("Feed cats");
     tasks.push_back("Brush teeth");
-    display(tasks);                 // test copy constructor
-    cout << "capacity():  " << tasks.capacity() << endl;
-    cout << "size():      " << tasks.size() << endl;
+    display(tasks);
+
+    // test size() function
+    cout << "size():     " << tasks.size() << endl;
+
+    // test pop_back() function
+    cout << "pop_back(): ";
+    tasks.pop_back();
+    display(tasks);
 
     // test at() function
-    cout << "at(0):       " << tasks.at(0) << endl;
-    cout << "at(size-1):  " << tasks.at(tasks.size() - 1) << endl;
-    // test resize() function
-    cout << "resize(20)\n";
-    tasks.resize(20);
-    cout << "capacity():  " << tasks.capacity() << endl;
-    cout << "size():      " << tasks.size() << endl;
+    cout << "at(0):      " << tasks.at(0) <<  endl;
+    cout << "at(size-1): " << tasks.at(tasks.size()-1) << endl;
+
+    // test remove() function
+    cout << "remove(tasks.size()-1):   ";
+    tasks.remove("Feed cats");
+    display(tasks);
+
+    // test size() function
+    cout << "size():     " << tasks.size() << endl;
 }
